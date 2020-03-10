116e948ed300200af2c754668d687c73de7d124f
gabidishcinger <gabrieladischinger@gmail.com>
2020-03-09 21:27:55 -0300

    Aula 14

---
+++ Aula14/Passaro.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula14
+{
+    //static void Main(string[] args)
+    //{
+
+    //    Passaro passaro = new Passaro();
+    //    passaro.BaterAsa();
+    //    passaro.Pousar();
+    //}
+
+    //classe abstrata é uma superclasse, que tem a base para outras
+    //classe abstrata só pode herdar de outras classes abstratas
+    //além disso, também não pode ser instanciada
+    //classe abstrata é usada para seguir um padrão
+
+    public abstract class Voar
+    {
+        public void BaterAsa()
+        {
+            Console.WriteLine("Bater asa da classe voar");
+        }
+
+        //método abstract não pode ser definido, implementado
+        public abstract bool Pousar();
+    }
+
+    public class Passaro : Voar
+    {
+
+        //métodos abstratos são obrigados a ser sobrecritos
+        public override bool Pousar()
+        {
+            Console.WriteLine("Pousar da classe passaro");
+            return true;
+        }
+    }
+}
---
+++ Aula14/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace CSharp_Modulo2
+{
+    class Program
+    {
+        static void Main()
+        {
+            LoginApp loginApp = new LoginApp();
+
+            string usuario = "";
+            string senha = "12345";
+
+            loginApp.FazerLogin(usuario, senha);
+            loginApp.FazerLogout();
+
+
+            LoginSite loginSite = new LoginSite();
+
+            string usuario2 = "gabi";
+            string senha2 = "123456";
+
+            loginSite.FazerLogin(usuario2, senha2);
+            loginSite.FazerLogout();
+            
+        }
+
+
+        //privado não é herdado
+        //privado realmente só pode ser utilizado dentro da classe a que pertence
+        //quando quer ser utilizado pelas classes filhas, mas não fora delas, usar protected.
+
+        public abstract class Login
+        {
+            public abstract void FazerLogin(string usuario, dynamic senha);
+
+            public abstract void FazerLogout();
+
+            protected virtual bool ValidarSeguranca(string usuario, dynamic senha)
+            {
+
+                if (usuario == "" && senha == "12345")
+                    return true;
+                else
+                    return false;
+
+            }
+        }
+
+        public class LoginApp : Login
+        {
+            public override void FazerLogin(string usuario, dynamic senha)
+            {               
+                
+                bool resultValidacao = ValidarSeguranca(usuario, senha);
+                if (resultValidacao)
+                    Console.WriteLine("Login realizado");
+                else
+                    Console.WriteLine("Login falhou");
+
+            }
+
+            public override void FazerLogout()
+            {
+                Console.WriteLine("Logout realizado");
+            }
+        }
+
+        public class LoginSite : Login
+        {
+            public override void FazerLogin(string usuario, dynamic senha)
+            {
+
+                bool resultValidacao = ValidarSeguranca(usuario, senha);
+                if (resultValidacao)
+                    Console.WriteLine("Sucesso");
+                else
+                    Console.WriteLine("Falha");
+
+            }
+
+            public override void FazerLogout()
+            {
+                Console.WriteLine("Saiu");
+            }
+
+            protected override bool ValidarSeguranca(string usuario, dynamic senha)
+            {
+                if (usuario == "" && senha == "123")
+                    return true;
+                else
+                    return false;
+            }
+        }
+
+    }
+}
